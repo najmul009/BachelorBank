40af5df52a5cf2dd4a5afb90a4e1946409fe1fd2
najmul009 <najmul.tk16@gmail.com>
2022-02-20 20:59:05 +0600

    error function added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ function getInputValue(field) {
     const getValue = parseFloat(strValue.value)
     return getValue;
 }
+//input error functio
+function inputError(error) {
+    const Warning = document.getElementById('Warning')
+    Warning.style.display = error
+}
+// balance error function
+function balanceError(error) {
+    const balanceWarning = document.getElementById('balance-Warning')
+    balanceWarning.style.display = error
+}

d6f165fda3887611c98b892a19876a54dff7ecb4
najmul009 <najmul.tk16@gmail.com>
2022-02-20 21:00:50 +0600

    event handelar done

--- js/script.js
+++ js/script.js
@@ -14,3 +14,65 @@ function balanceError(error) {
     const balanceWarning = document.getElementById('balance-Warning')
     balanceWarning.style.display = error
 }
+let getBalance = 0
+//event handaler on claculate btn
+document.getElementById('claculate-btn').addEventListener('click', function () {
+    const income = getInputValue('income-input')
+    const food = getInputValue('food-input')
+    const rent = getInputValue('rent-input')
+    const cloths = getInputValue('clothes-input')
+
+    // input error check 
+    if (income > 0 && food >= 0 && rent >= 0 && cloths >= 0) {
+        let totalExpenses = food + rent + cloths;
+        let balance = income - totalExpenses
+        getBalance = balance
+        //balance error check
+        if (balance >= 0) {
+            const totalExpensesText = document.getElementById('total-expenses')
+            const balanceText = document.getElementById('balance')
+
+            totalExpensesText.innerText = totalExpenses;
+            balanceText.innerText = balance
+            balanceError('none')
+        }
+        else{
+            balanceError('block')
+            inputError('none')
+        }
+
+        inputError('none')
+
+    } else {
+        inputError('block')
+        balanceError('none')
+    }
+
+})
+
+//event handaler on saving btn
+document.getElementById('saving-btn').addEventListener('click', function () {
+    const savePersent = getInputValue('saving-input')
+    const income = getInputValue('income-input')
+
+    //saving % input error check 
+    if (savePersent >= 0 && savePersent <= 100 && income > 0) {
+        let saving = (income * savePersent) / 100
+        let remainingBlacne = getBalance - saving;
+        const savingAmount = document.getElementById('saving-amount')
+        const remainingBlacneNew = document.getElementById('remaining-balance')
+        //saving balance error check
+        if(getBalance >= saving){
+        savingAmount.innerText = saving
+        remainingBlacneNew.innerText = remainingBlacne
+        inputError('none')
+        balanceError('none')
+        }
+        else{
+            balanceError('block')
+        }
+    } else {
+        inputError('block')
+        balanceError('none')
+    }
+})
